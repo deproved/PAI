b7fa73fafbe0a3e491ba0b650b861a75c9282133
Zakhar Holubov <zholubov@technikumlodz.pl>
2026-09-10 15:02:04 +0200

    zadanie 3

--- Zadanie_3_git/index.php
+++ Zadanie_3_git/index.php
@@ -1,3 +1,11 @@
 <?php
-    echo "hello world";
+    $a = 1;
+    $b = -5;
+    $c = 6;
+
+    $delta = $b**2 - 4 * $a * $c;
+
+    if ($delta < 0) echo "zero miejsc zerowych"; 
+    elseif ($delta > 0) echo "dwa miejsca zerowych x1: " . (-$b - sqrt($delta)) / (2 * $a) . " x2: " . (-$b + sqrt($delta)) / (2 * $a);
+    else echo "zero miejsc zerowych x0: " . -$b / (2 * $a);
 ?>
